0ada4b4fffe1a6919b1282e4bb0292cdd5740cb8
Francisco Eduardo <franciscoferreira88@gmail.com>
2018-09-23 14:31:00 -0300

    Add alias to composer

--- alias.sh
+++ alias.sh
@@ -1,12 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "$BASEDIR";
 
-if(![ -x "$(command -v php)" ])
+if [ ! -x "$(command -v php)" ]; then
 	alias php='docker run -ti --rm --name codephp -v "$PWD":/servidor/tmp -w /servidor/tmp  php:7.1 php'
+fi
+
+if [ ! -x "$(command -v composer)" ]; then
+	function composer() {
+		docker run -ti --rm --name codephp -v "$PWD":/servidor/tmp -w /servidor/tmp  php:7.1 php /servidor/composer.phar "$1-"
+	}
+	
+fi
 
 alias bash_php="docker-compose -f $BASEDIR/docker-compose.yml exec php71 bash"
 alias bash_mysql="docker-compose -f $BASEDIR/docker-compose.yml exec db bash"
-
--- image/install.php
+++ image/install.php
@@ -6,24 +6,24 @@ function imprimir($mensagem) {
 
 
 if( isset($argv[1]) && $argv[1] == "install" ) {
-	imprimir("Gerando backup das configurações padrões...");
+	imprimir("Copy backup default config files...");
 	system("cp -R /etc/apache2/ /servidor/apache2/");
 	system("cp -R /etc/php/ /servidor/php/");
-	imprimir("backup gerado.");
+	imprimir("backup copied.");
 }  else {
 
-	imprimir("Carregando arquivos de configuração...");
+	imprimir("Loading config files...");
 
 	$arquivosApache = new FilesystemIterator('/etc/apache2/', FilesystemIterator::SKIP_DOTS);
-	if(iterator_count($arquivosApache) <= 0) {
+	if(iterator_count($arquivosApache) <= 1) {
 		system("cp -R /servidor/apache2/* /etc/apache2/ ");
 	}
 	$arquivosApache = new FilesystemIterator('/etc/php/', FilesystemIterator::SKIP_DOTS);
-	if(iterator_count($arquivosApache) <= 0) {
+	if(iterator_count($arquivosApache) <= 1) {
 		system("cp -R /servidor/php/* /etc/php/ ");
 	}
 
-	imprimir("Arquivos carregados");
+	imprimir("Files loaded");
 
 	system("/usr/sbin/apache2ctl -D FOREGROUND");
 }
